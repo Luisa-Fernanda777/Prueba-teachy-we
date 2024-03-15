67296080fc2b01f072a361e4717decef49bb7de4
Alejo <dasd-123@hotmail.com>
2024-03-14 19:38:14 -0500

    First Commit

--- construirTriangulo/Program.cs
+++ construirTriangulo/Program.cs
@@ -2,12 +2,12 @@
 {
     public static bool PuedeFormarTriangulo(int a, int b, int c)
     {
-        // Comprobar si los lados son mayores que cero
+        // Se comprueba que los lados sean positivos
         if (a <= 0 || b <= 0 || c <= 0)
         {
             return false;
         }
-        // Comprobar la desigualdad triangular
+        // Se comprueba que la suma de dos lados sea mayor que el tercero o desigualdad triangular
         return a + b > c && a + c > b && b + c > a;
     }
 
@@ -17,7 +17,6 @@
         int lado2 = 4;
         int lado3 = 5;
 
-        // Llamada al método
         bool resultado = Triangulo.PuedeFormarTriangulo(lado1, lado2, lado3);
         Console.WriteLine("¿Se puede formar un triángulo? " + resultado);
     }
